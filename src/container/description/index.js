332a2d7c189b66615419a8cb5a77979c256faaa1
dev-reactjs <dev.reactjs@gmail.com>
2022-05-26 13:48:30 +0530

    heigth changes

--- src/container/description/index.js
+++ src/container/description/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate,useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
@@ -7,36 +7,49 @@ import Detail from "../../component/detail";
 import "./description.css";
 
 const Description = () => {
-const navigate = useNavigate();
-const {state} =useLocation()
-const handleClick = () => navigate("/");
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  const handleClick = () => navigate("/");
 
   return (
-    <Box sx={{ bgcolor: "background.default", height: '200vh', width: "100%", pr: 8, pl: 8, pt:8 }}>
-      <Button variant="contained" sx={{ mt: 6 }} startIcon={<ArrowBackIcon />} onClick={handleClick}>
+    <Box
+      sx={{
+        bgcolor: "background.default",
+        height: { xs: "200vh", sm: "200vh", md: "100%", lg: "100%" },
+        width: "100%",
+        pr: 8,
+        pl: 8,
+        pt: 8,
+      }}
+    >
+      <Button
+        variant="contained"
+        sx={{ mt: 6 }}
+        startIcon={<ArrowBackIcon />}
+        onClick={handleClick}
+      >
         back
       </Button>
       <div className="section">
-
-      <Box
-    component="img"
-    sx={{
-      width: {xs:300, sm: 400, md: 600 ,lg:700},
-      height: {xs:150, sm: 200, md: 400, lg:500},
-        content: {
-            xs: `url(${state?.flags?.png})`,
-            sm: `url(${state?.flags?.png})`,
-            md: `url(${state?.flags?.png})`, 
-            lg: `url(${state?.flags?.png})`, 
-        }
-    }}
-    alt="image"
-/>
+        <Box
+          component="img"
+          sx={{
+            width: { xs: 300, sm: 400, md: 600, lg: 600 },
+            height: { xs: 150, sm: 200, md: 400, lg: 400 },
+            content: {
+              xs: `url(${state?.flags?.png})`,
+              sm: `url(${state?.flags?.png})`,
+              md: `url(${state?.flags?.png})`,
+              lg: `url(${state?.flags?.png})`,
+            },
+          }}
+          alt="image"
+        />
         {/* <img
           src={state?.flags?.png}
           className="image"
         /> */}
-      <Detail data={state}/>
+        <Detail data={state} />
       </div>
     </Box>
   );
